618434b3e1625a3d29190b224a7570739a2f6b3c
RedDotwtt <velnychuk.dmytro@chnu.edu.ua>
2023-05-17 23:09:47 +0300

    1

---
+++ JS/nodes.js
@@ -0,0 +1,110 @@
+// Масив об'єктів із зображеннями та описом
+const nodes = [
+  {
+    src: "./Photos/11.jpg",
+    alt: "Магнезія в таблетках (Natures Bounty)",
+    description: "Дозування 500мг., 100 таблеток",
+    price: 750,
+    filter_setting: "magnesium",
+    id: 0,
+  },
+
+  {
+    src: "./Photos/6.jpg",
+    alt: "Магнезія в таблетках (Magnesia sitrat 300)",
+    description: "Дозування 300мг., 160 таблеток",
+    price: 800,
+    filter_setting: "magnesium",
+    id: 1,
+  },
+
+  {
+    src: "./Photos/12.jpeg",
+    alt: "Протеїн порошок (Sport Expert), смак: полуниця",
+    description: "Упаковка 908г.",
+    price: 2290,
+    filter_setting: "gainer",
+    id: 2,
+  },
+
+  {
+    src: "./Photos/4.jpg",
+    alt: "Магнезія розчинний напій (Optisona)",
+    description: "Дозування 200мг., 20 таблеток",
+    price: 150,
+    filter_setting: "magnesium",
+    id: 3,
+  },
+
+  {
+    src: "./Photos/5.jpg",
+    alt: "Магнезія в таблетках (Magnesia sitrat 375)",
+    description: "Дозування 375мг., 140 таблеток",
+    price: 800,
+    filter_setting: "magnesium",
+    id: 4,
+  },
+
+  {
+    src: "./Photos/2.jpg",
+    alt: "Протеїн порошок (Platinum Hydro Whey), смак: без смакових добавок",
+    description: "Упаковка 1100г.",
+    price: 2500,
+    filter_setting: "gainer",
+    id: 5,
+  },
+
+  {
+    src: "./Photos/7.jpg",
+    alt: "Набір з трьох банок протеїну (Gold Standart Whey)",
+    description: "Три упакрвки по 800г., смак: тропічна насолода, полуниця в шоколаді, карамель з ваніллю",
+    price: 5000,
+    filter_setting: "gainer",
+    id: 6,
+  },
+
+  {
+    src: "./Photos/8.jpg",
+    alt: "Протеїн порошок (Golden Whey) смак: melon icecream",
+    description: "упаковка 600гр.",
+    price: 1100,
+    filter_setting: "gainer",
+    id: 7,
+  },
+
+  {
+    src: "./Photos/9.jpg",
+    alt: "Магнезія розчинний напій (Mivolis)",
+    description: "Дозування 200мг., 20 таблеток",
+    price: 219,
+    filter_setting: "magnesium",
+    id: 8,
+  },
+
+  {
+    src: "./Photos/10.jpg",
+    alt: "Протеїн порошок (Vegan Proteine) смак: Bluebery cheesecake",
+    description: "Упаковка 1000гр.",
+    price: 2090,
+    filter_setting: "gainer",
+    id: 9,
+  },
+
+  {
+    src: "./Photos/1.jpg",
+    alt: "Протеїн порошок (Gold Standart Whey) смак: Lemon",
+    description: "Упаковка 1000гр.",
+    price: 3000,
+    filter_setting: "gainer",
+    id: 10,
+  },
+
+  {
+    src: "./Photos/3.jpg",
+    alt: "Магнезія в таблетках (Now)",
+    description: "Упаковка 180 таблеток, дозування: 400мг.",
+    price: 560,
+    filter_setting: "magnesium",
+    id: 11,
+  },
+];
